c1fbb0a1c9f29497f65758763a2664f4ba82137e
MuEbrahim <muhammedebrahim00@gmail.com>
2021-10-29 14:13:26 +0200

    New Features

--- src/App.js
+++ src/App.js
@@ -29,11 +29,12 @@ class App extends Component{
   }
 
   render(){
+    console.log(this.state.weather)
     return (
       <div className="App" style={{background:' url("imgs/weather.jpg")', backgroundSize:'cover', minHeight:'100vh'}}>
-        <ToastContainer position="top-center" autoClose={2000} />
+        <ToastContainer position="top-center" autoClose={2000}/>
         <div className='row mx-0 search-box'>
-            <input className="form-control col-5 my-3 mx-auto" type="search" placeholder="Search" aria-label="Search" 
+            <input className="form-control col-5 my-3 mx-auto" type="search" placeholder="Enter your city please." aria-label="Search" 
               onChange={(e)=>this.setState({cityName:e.target.value})}
               onKeyPress={(e) => this.searchHandler(e)}/>
         </div>
@@ -43,16 +44,40 @@ class App extends Component{
             <div className='text-center' style={{color:'white'}}>
               <div className='city-box'>
                 <p className='font-weight-bold' style={{fontSize:'25px'}}>{`${this.state.weather.name}, ${this.state.weather.sys.country}`}</p>
+                <p className='font-weight-bold' style={{fontSize:'25px'}}>{this.state.weather.weather[0].description}</p>
 
               </div>
               <div className='row mx-0 px-5'>
                 <div className='p-5 mx-auto shadow' style={{backgroundColor:'rgba(1,1,1,0.5)'}}>
+                  <p className='font-weight-bolder' style={{fontSize:'40px'}}>Temperature</p>  
                   <p className='font-weight-bolder' style={{fontSize:'40px'}}>{Math.round(this.state.weather.main.temp)}°C</p>  
+                  <div className='row'>
+                    <div className ='col-4'>
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>Min Temperature</p>  
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>{this.state.weather.main.temp_min}°C</p>  
+                    </div>  
+                    <div className ='col-4'>
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>Max Temperature</p>  
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>{this.state.weather.main.temp_max}°C</p>  
+                    </div>  
+                    <div className ='col-4'>
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>Feels like</p>  
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>{this.state.weather.main.feels_like}°C</p>  
+                    </div>  
+                    <div className ='col-4'>
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>Humidity</p>  
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>{Math.round(this.state.weather.main.humidity)}%</p>  
+                    </div>  
+                    <div className ='col-4'>
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>wind</p>  
+                      <p className='font-weight-bolder' style={{fontSize:'20px'}}>{this.state.weather.wind.speed}km/h</p>  
+                    </div>  
+                  </div>
                 </div>
               </div>
             </div>
           )
-        :(<p className='font-weight-bold mt-3 text-center' style={{fontSize:'30px',color:'white'}}>Enter City Please.</p>
+        :(<p className='font-weight-bold mt-3 text-center' style={{fontSize:'30px',color:'white'}}>Enter your city and press Enter.</p>
          )
         }
       </div>
